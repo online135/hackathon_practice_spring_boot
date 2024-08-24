ea132a2f7b6c3257c20e425b576c6c41615729d7
DESKTOP-4MPHLE6\User <b97b01067@gmail.com>
2024-08-24 09:26:39 +0800

    資料格式調整, enum 追加

--- src/main/java/com/example/demo/DataController.java
+++ src/main/java/com/example/demo/DataController.java
@@ -22,23 +22,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class DataController {
 	
     @GetMapping
-    @RequestMapping("/api/data")
+    @RequestMapping("/api/issues")
     public ResponseEntity<List<Map<String, Object>>> getData() {
         List<Map<String, Object>> data = new ArrayList<>();
 
         Map<String, Object> item1 = new HashMap<>();
-        item1.put("title", "Item 1");
-        item1.put("description", "This is the first item.");
+        item1.put("category", Category.A);
+        item1.put("title", "TestTitle1");
+        item1.put("date", "2024-08-22");
+        item1.put("description", "This is the 1 item.");
+        item1.put("status", Status.A);
+
         data.add(item1);
 
         Map<String, Object> item2 = new HashMap<>();
-        item2.put("title", "Item 2");
-        item2.put("description", "This is the second item.");
+        item2.put("category", Category.B);
+        item2.put("title", "TestTitle2");
+        item2.put("date", "2024-08-24");
+        item2.put("description", "This is the 2 item.");
+        item2.put("status", Status.B);
+
         data.add(item2);
 
         Map<String, Object> item3 = new HashMap<>();
-        item3.put("title", "Item 3");
+        item3.put("category", Category.C);
+        item3.put("title", "TestTitle3");
+        item3.put("date", "2024-08-24");
         item3.put("description", "This is the third item.");
+        item3.put("status", Status.C);
+
         data.add(item3);
 
         return new ResponseEntity<>(data, HttpStatus.OK);
